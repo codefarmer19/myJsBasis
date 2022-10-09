8fb3786bf3417d29e83c06b7754ad4ac2fb86c1a
codefarmer19 <86350437+codefarmer19@users.noreply.github.com>
2022-10-09 17:54:47 +0800

    a lot

--- index.js
+++ index.js
@@ -300,3 +300,220 @@ var mes = '';
     for (var i in nameqqq){
         console.log(nameqqq[i]);
     }
+
+    console.log('===================方法===========================');
+    function fun (){
+        console.log("这是方法");
+    }
+    console.log("执行方法",fun);
+    fun();
+
+    console.log('===================方法使用参数===========================');
+    function fun1 (canshu,canshu1){//这里括号里是变量，一个变量只会输出一个对应值
+        //这里是有一个参数来存想要输出的东西
+        console.log("这是方法fun1"+canshu+canshu1);
+    }
+    console.log("执行方法",fun1);
+    fun1("canshu","999");
+    console.log('===================方法返回return===========================');
+    var canshu2 = 999;
+    function fun3(canshu2){
+        if(canshu2 = 999){
+            return "它等于了";
+        }
+    }
+    let jieguuo = fun3();
+    console.log("jieguo",jieguuo);
+    fun3(canshu2);
+    // let result1 = fun2(canshu6);
+    // console.log("返回结果",result1);
+    
+
+
+    var ceshiFun = 12;
+    var ceshiFun1 = 1;
+    // function ceshiFun0 (ceshiFun,ceshiFun1) {
+        
+    //     let ceshiFun2 = ceshiFun+ceshiFun1;
+    //     console.log("方法2",ceshiFun2);
+    //     // return ceshiFun2;
+        
+    // }
+    // let ceshiFun3 = ceshiFun0(ceshiFun2);
+    // ceshiFun0();
+    // console.log("测试方法",ceshiFun3);
+
+
+    console.log('===================箭头方法===========================');
+    const add = (a,b) =>a+b;
+    console.log(add(1,2));
+
+
+    const jian = (a,c) => {
+        const result = a-c;
+        return result;
+    };
+    console.log(jian(10,1));
+
+    console.log('===================JSON===========================');
+    console.log('===================去浏览器复制链接看数据示例 这里做了筛选===========================');
+    console.log('===================搭配apipost===========================');
+    function tinqijiekou() {
+		$.ajax({
+			url:'https://www.yiketianqi.com/free/week?appid=82923341&appsecret=ZFtS7yHg&unescape=1&cityid=101310302,101310303,101310304',
+			type:"get",
+			dataType:"json",
+			success: function (response) {
+				var Str ="";
+				var tq = $('tbody[dom-type="table-listsb"]');
+				tq.children().remove();
+				
+				var x=0;
+				while (x<=2){
+					
+					var datas = response[x].data;
+
+					var b=1;
+					while(b<=1){
+						var Str ='<tr>';
+						Str += '<td>'+response[x].city+ '</td>';
+						Str += '<td>'+datas[b].date + '</td>';
+						Str += '<td>'+datas[b].wea + '</td>';
+						Str += '<td>'+datas[b].win + '</td>';
+						Str += '<td>'+datas[b].win_speed + '</td>';
+						Str += '</tr>'
+						tq.append(Str);
+						b++;
+					}
+					x++;
+				}
+				
+			}
+			
+		})
+	}
+    // <tbody dom-type="table-listsb">
+	// 							<tr>
+	// 								<td style="text-align:center" class="am-text-primary am-text-lg" colspan="7">
+    //                                     <strong>无数据</strong></td>
+	// 							</tr>
+								
+	// 						</tbody>
+    console.log('===================注释掉的是如何引用数据===========================');
+    [
+        {
+            "cityid": "101310302",
+            "city": "西沙",//这里就是response[x].city
+            "data": [//这里就是response[x].data 然后可以循环引用他下面的数据
+                {
+                    "date": "2022-10-09",//datas[b].date
+                    "wea": "雷阵雨",
+                    "wea_img": "yu",
+                    "tem_day": "31",
+                    "tem_night": "26",
+                    "win": "东北风",
+                    "win_speed": "4-5级转5-6级"
+                },
+                {
+                    "date": "2022-10-10",
+                    "wea": "雷阵雨",
+                    "wea_img": "yu",
+                    "tem_day": "31",
+                    "tem_night": "26",
+                    "win": "东北风",
+                    "win_speed": "5-6级"
+                }
+            ]
+        }
+    ]
+
+    console.log('===================结果示例===========================');
+
+    console.log('===================对象===========================');
+    const blank = {};//一个简单的对象由花括号组成
+    console.log("blank 的类型是",typeof blank);//输出是object就是对象
+    console.log("blank 的值是",blank);
+
+    console.log('===================对象提供属性===========================');
+    const book = {//这里面就是他的键值对
+        title:"1984",//数字
+        author:"作者",//字符串
+        isAvailable:false//布尔值 （小提示：最后一个键值对不用加逗号）
+    };
+    console.log("book 的类型是",typeof book);
+    console.log("book 的值是:",book);
+
+    console.log('===================对象中定义方法===========================');
+    const book1 = {//这里面就是他的键值对 //三个属性两个方法
+        title:"1984",//数字
+        author:"作者",//字符串
+        isAvailable:false,//布尔值 
+        chekIn:function(){ this.isAvailable = true},//这里给对象定义两个方法
+        chekOut : function(){ this.isAvailable = false}
+    };
+    console.log("book1 type",typeof book1);
+    console.log("book1 value:",book1);
+
+    console.log('===================轻量化对象===========================');
+    console.log('===================用关键字new创建对象实例===========================');
+    const book2 = new Object();//使用new关键字创建对象实例
+    console.log("定义book2");
+    console.log("book1 type",typeof book2);
+    console.log("book1 value:",book2);
+    const data = {title:"123",author:"作者"};
+    const book3 = new Object(data);//把data的值赋给新的book3
+    console.log('===================完整示例===========================');
+    const dataFunc = {//这里面就是他的键值对 
+        title:"1984",//数字
+        author:"作者",//字符串
+        isAvailable:false,//布尔值 
+        chekIn:function(){ this.isAvailable = true},//这里给对象定义两个方法
+        chekOut : function(){ this.isAvailable = false}
+    };
+    const book0 = new Object(dataFunc);
+    console.log("定义book0和dataFunc");
+    console.log("book0 type",typeof book0);
+    console.log("book0 value:",book0);
+    console.log("dataFunc type",typeof dataFunc);
+    console.log("dataFunc value:",dataFunc);
+    console.log('===================如何访问上面的三个属性和两个方法===========================');
+    //使用点表示法 .后面是属性名字
+    console.log(book0.title);//比如打印出title
+    //改变值
+    book0.author="作者在这里被改变了";
+    console.log(book0.author);
+
+    console.log('===================分割线===========================');
+    //使用括号表示法
+    console.log(book0["title"]);//比如打印出title
+    //改变值
+    book0["author"]="作者在这里被改变了";
+    console.log(book0);
+
+    console.log('===================分割线===========================');
+    //用在方法上
+    console.log(book0.isAvailable);
+    //调用方法
+    book0.chekOut();//在这里调用方法把isAvailable的值改成false
+    console.log(book0);
+    //括号表示法不写了 实操用不上
+    console.log('===================this关键字的讲解===========================');
+    console.log('===================他是函数上下文的句柄===========================');
+    console.log('案例1有个函数其中定义对象当使用他的时候返回this');
+    console.log('案例1 this实际就是bookobj的本身');
+    const bookobj = {
+        chekIn:function(){
+            return this;
+        }
+    }
+    console.log("这是",bookobj.chekIn());//这句返回的就是对象定义本身
+    console.log(bookobj.chekIn() === globalThis); //如果改成bookobj 等于true
+    console.log('案例2有个函数定义在全局作用域');
+    function anotherCheckIn(){
+        return this;
+    }
+    console.log("案例2:", anotherCheckIn());
+    console.log(anotherCheckIn() === globalThis);//在浏览器李这里全局对象是window
+
+    console.log('===================Promise===========================');
+    console.log('工作原理：new一个promise 传入一个回调函数 成功时调用第一个值 失败调用第二个');
